eaccf0e9f8172bb8c53220f133bc720785505113
basti92kubuntu <sebastian.ruf@posteo.de>
2014-03-25 13:52:09 +0100

    src

---
+++ src/tweetzAnalysis/Main.java
@@ -0,0 +1,122 @@
+package tweetzAnalysis;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import twitter4j.Query;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+
+public class Main implements Observer {
+
+    /**
+     * Total number of requests to the servers of Twitter
+     */
+    private int requests = 1;
+    /**
+     * number of current request
+     */
+    private int j = 0;
+
+    public static void main(String[] args) throws Exception {
+        // Initialize tweetzAnalysis.Main class
+        Main main = new Main();
+        // get tweetzAnalysis.TweetsProvider
+        TweetsProvider provider = TweetsReader.getInstance();
+        // register Observer that is notified whenever a sample of Tweets is
+        // available
+        provider.addObserver(main);
+        // initialize the tweetzAnalysis.TweetsProvider with the "login" data
+        provider.init("*******************",
+                "********************",
+                "****************",
+                "*************");
+        // initialize a query used to filter the incoming data stream
+        Query query = new Query("AcDc");
+        // set language to filter
+        query.setLang("en");
+        // start loading the samples from the server...
+        // hint: use null as input for loadSamples-method in order to access any Tweets
+        // to know: Use Query query = new Query() without any configurations may
+        // cause an exception!
+        provider.loadSample(query);
+    }
+
+    @Override
+    public void update(Observable arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        // state determines who (tweetzAnalysis.TweetsProvider or tweetzAnalysis.Timer-Thread) invoked that
+        // method
+        int state;
+        state = (int) arg1;
+        TweetsProvider provider = TweetsReader.getInstance();
+        switch (state) {
+            case TweetsReader.LOADED_TWEETS_AVAILABLE:
+                // the Tweets are saved in the statuses array
+                Status[] statuses = provider.getSample();
+                // do something
+                int i = 0;
+                while (statuses[i] != null) {
+                    i++;
+                }
+                Status tweet = statuses[0];
+                if (tweet != null) {
+                    String userData = "Tweet ist null..";
+                    if (tweet.getUser() != null) {
+                        userData = "\nAuthor: "+ tweet.getUser().getName() +"\nPlace: "+ tweet.getUser().getLocation() +
+                                "\nLang: "+ tweet.getUser().getLang() +"\n";
+                    }
+                    System.out.println("TweetId: "+ tweet.getId() +"\nText: "+ tweet.getText() + userData);
+                } else {
+                    System.out.println("Tweet ist Null..");
+                }
+                System.out.println("Number of Tweets in lap "+ j +": "+ i);
+                if (++j < requests) {
+                    // call a new tweetzAnalysis.Timer Thread that will signal the update method
+                    // (flag WAITING_FINISHED), if further Tweets
+                    // can be obtained or rather the 15 minute (+ 2 safety margin!)
+                    // window is over.
+                    waitForMillis(17 * 60000);
+                } else {
+                    // close data stream to Twitter..
+                    provider.close();
+                    System.exit(0);
+                }
+                break;
+            case Timer.WAITING_FINISHED:
+                try {
+                    // start loading another sample...
+                    // WARNING! Do not call the loadSample method before
+                    // WAITINIG_FINISHED has been signaled!
+                    // That may cause an exception, because there is a 15 minute
+                    // window to wait,
+                    Query query = new Query("AcDc");
+                    query.setLang("en");
+                    provider.loadSample(query);
+                } catch (TwitterException e) {
+                    // TODO Auto-generated catch block
+                    System.out.println("Exception: " + e.getMessage());
+                    System.out.println("Cause: " + e.getCause());
+                    System.out.println("cause is rate limit exceeded: "
+                            + e.exceededRateLimitation());
+                }
+                break;
+        }
+    }
+
+    /**
+     * Calls a new instance of tweetzAnalysis.Timer Thread, which waits for specified time.
+     *
+     * @param millis
+     *            time
+     */
+    private void waitForMillis(int millis) {
+        Timer timer = new Timer(millis, false);
+        timer.addObserver(this);
+        timer.start();
+    }
+
+
+
+}
+
---
+++ src/tweetzAnalysis/TweetsReader.java
@@ -0,0 +1,232 @@
+package tweetzAnalysis;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Set;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.RateLimitStatus;
+import twitter4j.RateLimitStatusEvent;
+import twitter4j.RateLimitStatusListener;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * Provides ONE standing connection to Twitter public streaming points used to
+ * obtain Tweets of several unknown users.
+ *
+ * @author Christopher Kerth
+ *
+ */
+public class TweetsReader extends Observable implements TweetsProvider,
+        RateLimitStatusListener {
+
+    /**
+     * Storage for the Tweets
+     */
+    private Status[] statuses;
+
+    public static final int MAX_TWEETS_PER_QUERY = 100;
+    public static final int MIN_TWEETS_PER_QUERY = 0;
+    private int numberOfTweetsPerRequest = MAX_TWEETS_PER_QUERY;
+
+    public static final int MAX_NUMBER_OF_REQUESTS = 180;
+    public static final int LOADED_TWEETS_AVAILABLE = 2;
+
+    /**
+     * Connection to Twitter public streaming endpoint
+     */
+    private Twitter twitterStream;
+    /**
+     * holdOn == true => statuses list is filled with
+     * -------------
+     * Tweets holdOn == false => currently no Tweets are recorded
+     */
+    private boolean holdOn;
+
+    private static TweetsReader instance;
+
+    private TweetsReader() {
+        statuses = new Status[MAX_NUMBER_OF_REQUESTS * MAX_TWEETS_PER_QUERY];
+    }
+
+    public static synchronized TweetsReader getInstance() {
+        if (instance == null) {
+            instance = new TweetsReader();
+        }
+        return instance;
+    }
+
+    // Implementation of interface tweetzAnalysis.TweetsProvider
+
+    @Override
+    public void init(String consumerKey, String consumerSecret,
+                     String accessToken, String accessTokenSecret) {
+        if (getTwitterStream() != null) {
+            return;
+        }
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret)
+                .setJSONStoreEnabled(true);
+        openTwitterStream(cb);
+    }
+
+    public void loadSample(Query query) throws TwitterException {
+        setHoldOn(false);
+        if (getTwitterStream() == null) {
+            return;
+        }
+        // reset old values retrieved from the previous query
+        for (int i = 0; i < statuses.length; i++) {
+            statuses[i] = null;
+        }
+        int pos = 0;
+        int oldSize = 0;
+        if (query == null) {
+            query = createDefaultQuery();
+        }
+        Set<Long> set = new HashSet<Long>();
+        query.setCount(getNumberOfTweetsPerRequest());
+        try {
+            QueryResult result = null;
+            if (!isHoldOn()) {
+                result = getTwitterStream().search(query);
+            }
+            if (result == null) {
+                return;
+            }
+            do {
+                List<Status> tweets = result.getTweets();
+                for (Status tweet : tweets) {
+                    long id = tweet.getId();
+                    oldSize = set.size();
+                    set.add(id);
+                    if (set.size() > oldSize) {
+                        oldSize = set.size();
+                        statuses[pos++] = tweet;
+                    }
+                }
+                // get tweets in the next page if any
+                query = result.nextQuery();
+                if (query != null) {
+                    result = getTwitterStream().search(query);
+                }
+            } while (!isHoldOn() && query != null);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        notifyChange();
+    }
+
+    private Query createDefaultQuery() {
+        String search = "";
+        for (int i = 65; i < 91; i++) {
+            search += " OR " + (char) i;
+        }
+        return new Query(search);
+    }
+
+    @Override
+    public Status[] getSample() {
+        // TODO Auto-generated method stub
+        return statuses;
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        // TODO Auto-generated method stub
+        super.addObserver(observer);
+    }
+
+    @Override
+    public boolean close() {
+        // TODO Auto-generated method stub
+        closeTwitterStream();
+        return true;
+    }
+
+    // Helper methods
+
+    /**
+     * Establishes a connection to the server using the provided settings.
+     *
+     * @param settings
+     *            OAuth consumer key, OAuth secret key, access token secret, ...
+     */
+    private void openTwitterStream(ConfigurationBuilder settings) {
+        Twitter twitter = new TwitterFactory(settings.build()).getInstance();
+        twitter.addRateLimitStatusListener(this);
+        setTwitterStream(twitter);
+    }
+
+    /**
+     * Closes the connection to the server.
+     */
+//    private void closeTwitterStream() {
+//        if (getTwitterStream() != null) {
+//           getTwitterStream().shutdown();
+//        }
+//    }
+
+    // Getters and Setters
+
+    private Twitter getTwitterStream() {
+        return twitterStream;
+    }
+
+    private void setTwitterStream(Twitter twitterStream) {
+        this.twitterStream = twitterStream;
+    }
+
+    private boolean isHoldOn() {
+        return holdOn;
+    }
+
+    private void setHoldOn(boolean holdOn) {
+        this.holdOn = holdOn;
+    }
+
+    public int getNumberOfTweetsPerRequest() {
+        return numberOfTweetsPerRequest;
+    }
+
+    @Override
+    public void onRateLimitReached(RateLimitStatusEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onRateLimitStatus(RateLimitStatusEvent arg0) {
+        // TODO Auto-generated method stub
+        RateLimitStatus rateLimit = arg0.getRateLimitStatus();
+        if (isHoldOn() && rateLimit.getRemaining() != rateLimit.getLimit()) {
+            return;
+        }
+        setHoldOn(false);
+        int remaining = rateLimit.getRemaining();
+        if (remaining == 2) {
+            notifyChange();
+        }
+    }
+
+    /**
+     * Invokes the update method of each Observer.
+     */
+    private void notifyChange() {
+        setHoldOn(true);
+        setChanged();
+        notifyObservers(LOADED_TWEETS_AVAILABLE);
+    }
+
+}
+
